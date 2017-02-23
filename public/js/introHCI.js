ba8fbf5f18dfed31a3a376057d69144ab2f425b0
Kaye Geronilla <kgeronil@ucsd.edu>
2017-02-23 10:20:01 -0800

    google analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,10 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(google);
+}
+
+function google(e){
+	ga("send", "event", 'like', 'click');
+	console.log("clicked");
 }
